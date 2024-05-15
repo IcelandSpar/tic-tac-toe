55a63c4389fcccd5aa5665ec70bcda3dba6aa434
IcelandSpar <victorasielrodriguez@gmail.com>
2024-05-15 08:57:03 -0500

    Add player input prompt to choose position in log
    player can now choose the position in the array by entering row and column

--- script.js
+++ script.js
@@ -2,16 +2,20 @@
 
 
 const Gameboard = (function(){
-    let space = [0];
+    let space = 0;
     let board = [];
 
     let row = 3;
     
-    
+    let rowChoice = 0;
+    let columnChoice = 0;
+    let player1 = 1;
+    let player2 = 2
+
     makeBoard = function() {
         
             for(i = 0; i < row; i++) {
-                this.board.push(space)
+                this.board.push([space, space, space])
 
             }
             
@@ -19,19 +23,45 @@ const Gameboard = (function(){
     };
     
     makeColumn = function() {
-        for(i = 1; i < board.length; i++) {
+        for(i = 1; i < 3; i++) {
           this.board[i].push(0)
         }
     };
 
+    playerChoice = function(player) {
+        rowChoice = prompt("row coordinate " + player);
+        columnChoice = prompt("column coordinate " + player);
 
-    return {makeBoard, board, makeColumn}
+
+    }
+
+    printChoice = function(player) {
+
+       board[rowChoice].splice(columnChoice,1)
+      board[rowChoice].splice(columnChoice, 0, player)
+    }
+
+
+    return {makeBoard, board, makeColumn, playerChoice, printChoice, player1, player2}
 
 })();
 
 
 Gameboard.makeBoard();
-Gameboard.makeColumn();
+
+// Gameboard.playerChoice(Gameboard.player1);
+// Gameboard.printChoice(Gameboard.player1);
+
+// Gameboard.playerChoice(Gameboard.player2);
+// Gameboard.printChoice(Gameboard.player2);
+
+// Gameboard.playerChoice(Gameboard.player1);
+// Gameboard.printChoice(Gameboard.player1);
+
+// Gameboard.playerChoice(Gameboard.player2);
+// Gameboard.printChoice(Gameboard.player2);
+
+
 
 console.log(Gameboard.board)
 
@@ -41,40 +71,36 @@ console.log(Gameboard.board)
 
 
 
+// const GameFlow = (function() {
+
+//     let player1 = "X";
+//     let player2 = "O";
 
 
+//     startGame = function() {
+//         alert("Starting Game, player 1 turn")
 
 
-const GameFlow = (function() {
+//     }
 
-    let player1 = "X";
-    let player2 = "O";
-    let rowChoice = 0;
-    let columnChoice = 0;
-
-    startGame = function() {
-        alert("Starting Game, player 1 turn")
+//     playerChoice = function() {
+//         rowChoice = prompt("row coordinate");
+//         columnChoice = prompt("column coordinate");
 
 
-    }
+//     }
 
-    playerChoice = function() {
-        rowChoice = prompt("row coordinate");
-        columnChoice = prompt("column coordinate");
+//     printChoice = function() {
 
-
-    }
-
-    printChoice = function() {
         
-
-    }
-
+//     }
 
 
-    return {startGame, playerChoice, rowChoice, columnChoice}
-})()
 
+//     return {startGame, playerChoice, rowChoice, columnChoice, printChoice}
+// })()
+
+// GameFlow.printChoice();
 
 
 
